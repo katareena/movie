dfaf79d1113df18875dee69bbd770d19b08ba991
katareena <w10160177@gmail.com>
2022-12-19 21:59:24 +0300

    removes useless div from add-component

--- src/components/add-button/add-button.tsx
+++ src/components/add-button/add-button.tsx
@@ -3,12 +3,10 @@ import './add-button.scss';
 
 const AddButton = (): JSX.Element => {
   return (
-    <div className='add'>
-      <button className='add__button' type='button'>
-        <span className='add__button-mark'></span>
-        <span className='add__button-text'>add movie</span>
-      </button>
-    </div>
+    <button className='add' type='button'>
+      <span className='add__button-mark'></span>
+      <span className='add__button-text'>add movie</span>
+    </button>
   );
 };
 
