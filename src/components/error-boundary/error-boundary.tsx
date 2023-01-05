4088681f5525ad797ed44f29a284718229e3cf9a
katareena <w10160177@gmail.com>
2023-01-05 15:33:29 +0300

    removes an unnecessary method from ErrorBoundary

--- src/components/error-boundary/error-boundary.tsx
+++ src/components/error-boundary/error-boundary.tsx
@@ -1,4 +1,4 @@
-import { Component, ErrorInfo, ReactNode } from 'react';
+import { Component, ReactNode } from 'react';
 
 interface Props {
   children?: ReactNode;
@@ -16,10 +16,6 @@ class ErrorBoundary extends Component<Props, State> {
   public static getDerivedStateFromError(): State {
     // Update state so the next render will show the fallback UI.
     return { hasError: true };
-  }
-
-  public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-    console.error('Uncaught error:', error, errorInfo);
   }
 
   public render() {
